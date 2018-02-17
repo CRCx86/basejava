54d9fa52aa36d6908349d4fb8c5ae64037f14f1d
CRCx86 <crc32zinov@gmail.com>
2018-02-18 00:21:22 +0300

    first comment

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,60 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (String string: storage) {
+            s = null;
+        }
     }
 
     void save(Resume r) {
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = r;
+            }
+        }
+
     }
 
     Resume get(String uuid) {
-        return null;
+
+        String result = null;
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == uuid){
+                result = storage[i];
+            }
+        }
+
+        return result;
     }
 
     void delete(String uuid) {
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == uuid){
+                storage[i] = null;
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+
+        Resume[] result = new Resume[10000];
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                result[i] = storage[i];
+            }
+        }
+
+        return result;
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
